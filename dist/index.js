807f119809697df3d95a3603ba40b80847e2eaf6
erdivartanovich <erdivartanovich@gmail.com>
2018-04-27 19:38:47 +0700

    export member

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-import { IProduct, ICartItem, ICartItems, ICartSummary, ICart } from './interfaces/index';
-export { IProduct, ICartItem, ICartItems, ICartSummary, ICart };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ICartItem, ICartItems, ICartSummary, ICart } from './interfaces/cart';
 import { IProduct } from './interfaces/product';
 import { IStorage } from './interfaces/storage';
+
 export { IProduct, ICartItem, ICartItems, ICartSummary, ICart, IStorage };
