9dca215eeb41f8d445455c932505ce9f993ffdb5
erdivartanovich <erdivartanovich@gmail.com>
2018-04-29 21:20:08 +0700

    export cart engine

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 import { ProductService } from './domains/product/product.service';
+import { CartService } from './domains/cart/cart.service';
 import { ProductModelFirestore } from './domains/product/product.model.firestore';
-export { ProductService, ProductModelFirestore };
+import { CartModelFirestore } from './domains/cart/cart.model.firestore';
+export { ProductService, ProductModelFirestore, CartService, CartModelFirestore };
