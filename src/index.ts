00d1df9500528f59196594c524a30286940f6713
erdivartanovich <erdivartanovich@gmail.com>
2018-04-29 21:18:38 +0700

    next release

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ProductService } from './domains/product/product.service';
 import { CartService } from './domains/cart/cart.service';
 import { ProductModelFirestore } from './domains/product/product.model.firestore';
+import { CartModelFirestore } from './domains/cart/cart.model.firestore';
 
-export {ProductService, ProductModelFirestore};
+export {ProductService, ProductModelFirestore, CartService, CartModelFirestore};
