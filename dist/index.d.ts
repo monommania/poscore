af1acda58623a493dc1380acd59aa8c771f7dd9f
erdivartanovich <erdivartanovich@gmail.com>
2018-05-05 17:29:03 +0700

    next release

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,7 @@
 import { ProductService } from './domains/product/product.service';
 import { CartService } from './domains/cart/cart.service';
+import { TransactionService } from './domains/transaction/transaction.service';
 import { ProductModelFirestore } from './domains/product/product.model.firestore';
 import { CartModelFirestore } from './domains/cart/cart.model.firestore';
-export { ProductService, ProductModelFirestore, CartService, CartModelFirestore };
+import { TransactionModelFirestore } from './domains/transaction/transaction.model.firestore';
+export { ProductService, ProductModelFirestore, CartService, CartModelFirestore, TransactionService, TransactionModelFirestore };
