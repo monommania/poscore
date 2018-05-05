6bc91e5142a64f9f3356f7a7bf0cbf9f226928a2
erdivartanovich <erdivartanovich@gmail.com>
2018-05-05 17:28:44 +0700

    export transaction module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { ProductService } from './domains/product/product.service';
 import { CartService } from './domains/cart/cart.service';
+import { TransactionService } from './domains/transaction/transaction.service';
 import { ProductModelFirestore } from './domains/product/product.model.firestore';
 import { CartModelFirestore } from './domains/cart/cart.model.firestore';
+import { TransactionModelFirestore } from './domains/transaction/transaction.model.firestore';
 
-export {ProductService, ProductModelFirestore, CartService, CartModelFirestore};
+export {
+    ProductService,
+    ProductModelFirestore,
+    CartService,
+    CartModelFirestore,
+    TransactionService, TransactionModelFirestore
+};
