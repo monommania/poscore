f4e600fa5cc7c2a5a8aa1711cd22ef8e4331cb90
erdivartanovich <erdivartanovich@gmail.com>
2018-05-06 16:31:10 +0700

    next release

--- dist/domains/transaction/transaction.model.firestore.js
+++ dist/domains/transaction/transaction.model.firestore.js
@@ -56,7 +56,7 @@ export class TransactionModelFirestore {
                 return product
                     .where('date', ">=", fromFilter)
                     .where('date', "<=", toFilter)
-                    .orderBy('date', 'desc')
+                    .orderBy('time', 'desc')
                     .get()
                     .then(function (snapshots) {
                     const data = [];
