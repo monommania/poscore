4aba254aca2cc51cce6362b2ccde237a75c51137
erdivartanovich <erdivartanovich@gmail.com>
2018-05-06 16:48:43 +0700

    change order by date interval

--- src/domains/transaction/transaction.model.firestore.ts
+++ src/domains/transaction/transaction.model.firestore.ts
@@ -55,6 +55,7 @@ export class TransactionModelFirestore {
                 return product
                     .where('date', ">=", fromFilter)
                     .where('date', "<=", toFilter)
+                    .orderBy('date', 'desc')
                     .orderBy('time', 'desc')
                     .get()
                     .then(function(snapshots) {
